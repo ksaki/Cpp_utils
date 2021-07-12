202a092cecd1d45e462723aeaefd2c478a375431
ksaki <sakikuzushima@gmail.com>
2021-07-12 12:12:57 -0700

    minor change

--- rcpp_copy.R
+++ rcpp_copy.R
@@ -6,15 +6,24 @@ sourceCpp("ref_modify.cpp")
 # Deep copy will occur when the values of the object is modified
 x <- c(1,2,3)
 y <- x
-cat("address of x and y identical before modifying by R", obj_addr(x) == obj_addr(y), "\n")
-x[1] <- 0
-cat("address of x and y identical after modifying by R", obj_addr(x) == obj_addr(y), "\n")
+cat("address of x and y identical before modifying by R",
+    obj_addr(x) == obj_addr(y), "\n")
+# TRUE
+x[1] <- 10
+cat("address of x and y identical after modifying by R",
+    obj_addr(x) == obj_addr(y), "\n")
+# FALSE
 
-# But, it will not occur when the values of the object is modified by Rcpp through refrence
+# But, it will not occur when the values of the object is modified 
+# by Rcpp through refrence
 n <- c(1,2,3)
 m <- n
-cat("address of n and m identical before modifying by Rcpp", obj_addr(n) == obj_addr(m), "\n")
+cat("address of n and m identical before modifying by Rcpp",
+    obj_addr(n) == obj_addr(m), "\n")
+# TRUE
 ref_modify(n)
-cat("address of n and m identical after modifying by Rcpp ", obj_addr(n) == obj_addr(m), "\n")
+cat("address of n and m identical after modifying by Rcpp ",
+    obj_addr(n) == obj_addr(m), "\n")
+# TRUE
 
 
--- ref_modify.cpp
+++ ref_modify.cpp
@@ -1,7 +1,7 @@
 // modify by refernce
 #include <Rcpp.h>
 
-
+//[[Rcpp::plugins(cpp11)]]
 //[[Rcpp::export]]
 void ref_modify(Rcpp::NumericVector& x){
   x(0) = 10; 
